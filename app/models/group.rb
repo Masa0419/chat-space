dddc438e425e23e44a371f04b4bba419854b5da4
masa <Masatoshi@Masatoshilocal.local>
2018-07-04 21:05:08 +0900

    [fix]最終正

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,7 @@ class Group < ApplicationRecord
 	has_many :messages
 
 	validates :name, presence: true
-	def show_last_message
+	def show_list_message
 	    if (last_message = messages.last).present?
 	      last_message.content? ? last_message.content : '画像が投稿されています'
 	    else
