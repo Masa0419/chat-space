10f60ecc4580857980b8eb664c80ee79929e9508
masa <Masatoshi@Masatoshilocal.local>
2018-07-06 19:25:53 +0900

    [add]テストの追加

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+
 describe MessagesController do
   let(:group) { create(:group) }
   let(:user) { create(:user) }
@@ -35,32 +36,55 @@ describe MessagesController do
       end
     end
   end
-
   describe '#create' do
     let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
 
     context 'log in' do
-    # この中にログインしている場合のテストを記述
       before do
         login user
       end
 
       context 'can save' do
-      # この中にメッセージの保存に成功した場合のテストを記述
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up message' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
       end
 
       context 'can not save' do
-      # この中にメッセージの保存に失敗した場合のテストを記述
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'renders index' do
+          subject
+          expect(response).to render_template :index
+        end
       end
     end
 
     context 'not log in' do
-    # この中にログインしていない場合のテストを記述
-      before do
-        get :create, params: params
-      end
 
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
-
 end
