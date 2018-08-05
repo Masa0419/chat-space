70e2169678ea3c96184a77f7779fa20e8066a1c2
masa <Masatoshi@Masatoshilocal.local>
2018-08-05 18:49:06 +0900

    [fix]インデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where("name LIKE(?) && #{params[:keyword].present?}","%#{params[:keyword]}%").where.not(name: current_user.name)
+    @users = User.where("name LIKE(?) && #{params[:keyword].present?}","%#{params[:keyword]}%").where.not(name: current_user.id)
     respond_to do |format|
       format.html
       format.json
