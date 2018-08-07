6272fc7482d43c9fd3dc42f6616e0ab8620a78ca
masa <Masatoshi@Masatoshilocal.local>
2018-08-07 20:18:15 +0900

    [fix]インデント修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,8 @@ class MessagesController < ApplicationController
   before_action :set_group
 
   def index
-  	@message = Message.new
-  	@messages = @group.messages.includes(:user)
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
 
     respond_to do |format|
       format.html
